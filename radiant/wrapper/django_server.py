10e9451835aab37c9195151e1cff25522274563e
Yeison <yencardonaal@unal.edu.co>
2025-08-31 11:46:26 -0500

    Refactor server wrapper and clean up unused code
    Renamed `server.py` to `django_server.py` for clarity and removed unused imports, comments, and separators. Improved `main()` function by allowing dynamic IP and port configuration, enhancing flexibility.

--- radiant/wrapper/django_server.py
+++ radiant/wrapper/django_server.py
@@ -1,12 +1,3 @@
-"""
-======
-Server
-======
-
-
-"""
-
-import sys
 import os
 import time
 import socketserver
@@ -14,7 +5,6 @@ import logging
 from wsgiref import simple_server
 from django.core.wsgi import get_wsgi_application
 from radiant.wrapper.android.permissions import Permission
-
 
 class ThreadedWSGIServer(socketserver.ThreadingMixIn, simple_server.WSGIServer):
     """Start Django in multithreaded mode
@@ -24,8 +14,6 @@ class ThreadedWSGIServer(socketserver.ThreadingMixIn, simple_server.WSGIServer):
     """
     pass
 
-
-# ----------------------------------------------------------------------
 def set_permissions(permissions):
     """"""
     _permissions = [getattr(Permission, permission) for permission in [permissions]]
@@ -38,12 +26,10 @@ def set_permissions(permissions):
             time.sleep(0.1)
 
 
-# ----------------------------------------------------------------------
 def main(project, ip='localhost', port=5000):
-    """"""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{project}.settings")
     os.environ.setdefault("FRAMEWORK", "django")
-    httpd = simple_server.make_server('localhost', 5000, get_wsgi_application(), server_class=ThreadedWSGIServer)
+    httpd = simple_server.make_server(ip, port, get_wsgi_application(), server_class=ThreadedWSGIServer)
     httpd.serve_forever()
     logging.info("Radiant serving on {}:{}".format(*httpd.server_address))
 
